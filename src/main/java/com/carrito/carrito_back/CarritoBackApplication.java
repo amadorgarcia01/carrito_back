88d658e656cdd52a2822a68ce9b745f7b7aa4486
Amador Garcia <amadorgarcia01@gmail.com>
2024-11-21 22:24:10 -0500

    Creación de la aplicación

---
+++ src/main/java/com/carrito/carrito_back/CarritoBackApplication.java
@@ -0,0 +1,13 @@
+package com.carrito.carrito_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritoBackApplication.class, args);
+	}
+
+}
